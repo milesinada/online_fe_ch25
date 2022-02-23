e0a24403482dd398262d489060684ed718050951
Miles Inada <milesinada219@gmail.com>
2022-02-22 20:52:20 -1000

    109 React class 2

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Thanks from "./components/footer";
 import Catalog from "./components/catalog";
 import About from "./components/about";
 import Home from "./components/home";
+import Cart from "./components/cart";
+import Admin from "./components/admin";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
@@ -20,6 +22,8 @@ function App() {
           <Route path="/" element={<Home />}></Route>
           <Route path="/catalog" element={<Catalog />}></Route>
           <Route path="/about" element={<About />}></Route>
+          <Route path="/cart" element={<Cart />}></Route>
+          <Route path="/admin" element={<Admin />}></Route>
         </Routes>
 
         <Thanks />
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,36 +1,64 @@
-import "./navBar.css"
-import {Link} from "react-router-dom";
+import "./navBar.css";
+import { Link } from "react-router-dom";
 //import GeoFind from"./weather.jsx"
 
-const NavBar = () =>{
-     //console.log ("Your current position is :"+ <GeoFind/>);
-    return(
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div className="container-fluid">
-    <a className="navbar-brand" to="#">Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/catalog">Catalog</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link " to="/about" tabIndex="-1">About</Link>
-        </li>
-      </ul>
-      <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-    );
-}
+const NavBar = () => {
+  //console.log ("Your current position is :"+ <GeoFind/>);
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" to="#">
+          Navbar
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/catalog">
+                Catalog
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link " to="/about" tabIndex="-1">
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link " to="/admin" tabIndex="-1">
+                Admin
+              </Link>
+            </li>
+          </ul>
+          <form className="d-flex">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default NavBar;
